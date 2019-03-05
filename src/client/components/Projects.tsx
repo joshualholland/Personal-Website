52ce716d201cc7c94d3fb4d4c602038dcc7d65c4
joshualholland <joshualholland@gmail.com>
2019-03-04 19:19:00 -0600

    added project

--- src/client/components/Projects.tsx
+++ src/client/components/Projects.tsx
@@ -8,7 +8,7 @@ const Projects = () => {
             <h1 className='text-primary mt-5'>Projects</h1>
             <hr className='bg-primary'></hr>
             <div>
-                <h3 className='text-danger'>Smash Bros. Ultimate Tournament Utility</h3>
+                <h3 className='text-primary'>Smash Bros. Ultimate Tournament Utility</h3>
                 <ul className='lang-ul'>
                     <li className='text-danger lang'>Built to help professional Smash Bros. Tournament Operators with running tournaments.</li>
                     <li className='text-danger lang'>Walks players through rules for stage striking and allows them to strike stages via my application.</li>
@@ -29,6 +29,23 @@ const Projects = () => {
                 <div className='col-md-4'>
                     <a className='mr-3' target='_blank' href='https://github.com/joshualholland/smashbros_tournament'><FontAwesomeIcon icon={faEye} /> View Source Code</a>
                     <a target='_blank' href='https://protected-caverns-96816.herokuapp.com/'><FontAwesomeIcon icon={faEye} /> View Live Demo</a>
+                </div>
+                <h3 className='text-primary mt-5'>Just My Type</h3>
+                <ul className='lang-ul'>
+                    <li className='text-danger lang'>A typing game that measures wpm.</li>
+                    <li className='text-danger lang'>Gives live feedback and css animations for knowing which letter to type.</li>
+                    <li className='text-danger lang'>Score is reported at the end and the user can choose to play again.</li>
+                </ul>
+                <img className='col-md-3 mr-3 p-0 sbu' src='images/JMT.png' alt='Typing game' height='200' width='300' />
+                <img className='col-md-3 p-0 sbu' src='images/JMT2.png' alt='Feedback' height='200' width='300' />
+                <div className='col-md-3' style={{ display: 'inline-block' }}>
+                    <span className='badge badge-primary text-secondary m-2'>jQuery</span>
+                    <span className='badge badge-primary text-secondary m-2'>HTML5</span>
+                    <span className='badge badge-primary text-secondary m-2'>CSS</span>
+                </div>
+                <div className='col-md-4'>
+                    <a className='mr-3' target='_blank' href='https://github.com/joshualholland/just-my-type'><FontAwesomeIcon icon={faEye} /> View Source Code</a>
+                    <a target='_blank' href='https://joshualholland.github.io/'><FontAwesomeIcon icon={faEye} /> View Live Demo</a>
                 </div>
 
             </div>
